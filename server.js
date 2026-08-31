5d78624158e3e38bfa68f5276f5dffcb276de664
Brujo Ryan <philipryandeal@gmail.com>
2026-08-31 05:08:37 -0400

    Redirect temporary Railway hostname to seraphnganga.com

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const CANONICAL_ORIGIN = 'https://seraphnganga.com';
+
+app.use((req, res, next) => {
+  const host = (req.get('host') || '').toLowerCase();
+
+  if (host.endsWith('.up.railway.app')) {
+    return res.redirect(301, `${CANONICAL_ORIGIN}${req.originalUrl}`);
+  }
+
+  next();
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
